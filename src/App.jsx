f16ef55ffa481db5b2f2c40e187fa95e0899f243
Danbi Choi <sweetrain05@hotmail.com>
2023-12-06 20:21:53 -0800

    Reducing state management

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,30 @@ import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 
+function deriveActivePlayer(gameTurns) {
+    let currPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currPlayer = 'O';
+    }
+
+    return currPlayer;
+}
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
-    const [activePlayer, setActivePlayer] = useState('X');
-
-    const handleSwitchPlayer = () => {
-        setActivePlayer((curr) => (curr === 'X' ? 'O' : 'X'));
-    };
+    const activePlayer = deriveActivePlayer(gameTurns);
 
     const handleBoardClick = (rowIndex, colIndex) => {
         setGameTurns((prev) => {
-            let currPlayer;
-            if (prev.length > 0 && prev[0].player === 'X') currPlayer = 'O';
-            else currPlayer = 'X';
+            const currPlayer = deriveActivePlayer(prev);
 
             const updatedGameTurns = [...prev];
             updatedGameTurns.unshift({
                 square: { row: rowIndex, col: colIndex },
                 player: currPlayer,
             });
-            console.log(updatedGameTurns);
+
             return updatedGameTurns;
         });
     };
